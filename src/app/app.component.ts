035ecdd8e3c98e22dfef671deeb2a0d7d67f64e4
alexander gou <395655863@qq.com>
2018-12-04 00:54:43 +0800

    defind componet self

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BlogHeaderComponent } from "src/app/components/index/blog-header/blog-header.component"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
